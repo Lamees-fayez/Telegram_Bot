b552cca11aba6787f61530cd2707af57cb9ed702
Lamees-fayez <lameesfayez@gmail.com>
2026-03-29 19:12:44 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 from database import JobsDatabase
 from typing import Dict
 
